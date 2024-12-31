dccb07513ef117b927cbcd048fee8aa664e3c343
pratikjatale <pjatale8@gmail.com>
2024-12-31 09:56:28 +0530

    add

---
+++ springpractice5/src/main/java/com/springboot/springpractice5/Springpractice5Application.java
@@ -0,0 +1,13 @@
+package com.springboot.springpractice5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springpractice5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springpractice5Application.class, args);
+	}
+
+}
---
+++ springpractice5/src/test/java/com/springboot/springpractice5/Springpractice5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.springpractice5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springpractice5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
